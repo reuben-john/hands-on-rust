2cf2c3b5a5e40a94edbefcf8374b2867bae57f26
Reuben Koppenheffer <reuben.koppenheffer@accredopkg.com>
2021-07-08 15:55:18 -0500

    Clean up name return

--- chapter-2/treehouse/src/main.rs
+++ chapter-2/treehouse/src/main.rs
@@ -3,9 +3,8 @@ use std::io::stdin;
 fn main() {
     println!("Hello, what's your name?");
     let your_name = what_is_your_name();
-    println!("Hello, {}", your_name)
+    println!("Hello, {}. It is a pleasure to meet you.", your_name) // You can debug your placeholder by using {:?}
 }
-
 
 /// This will return a string
 fn what_is_your_name() -> String {
@@ -14,7 +13,9 @@ fn what_is_your_name() -> String {
         // Requires creating an empty string in order to save to it.
         // &mut indicates you are borrowing the variable in order to update it
         // prefixing with & creates a reference to a variable.  This allows you to pass the reference instead of a copy
-        .read_line(&mut your_name) 
+        .read_line(&mut your_name)
         .expect("Failed to read line"); // Show this if it fails
-    your_name // Return statement is the final item in a function
+    your_name // Return statement doesn't end in a semi colon
+        .trim() // remove extra characters
+        .to_lowercase()  // convert to lowercase
 }
