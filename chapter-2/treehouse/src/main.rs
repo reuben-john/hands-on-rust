9a40dc2f7d2c6cd8f521e099ea36b1b246dda351
Reuben Koppenheffer <reuben.koppenheffer@accredopkg.com>
2021-07-08 15:50:40 -0500

    Seperate name asking function

--- chapter-2/treehouse/src/main.rs
+++ chapter-2/treehouse/src/main.rs
@@ -2,6 +2,13 @@ use std::io::stdin;
 
 fn main() {
     println!("Hello, what's your name?");
+    let your_name = what_is_your_name();
+    println!("Hello, {}", your_name)
+}
+
+
+/// This will return a string
+fn what_is_your_name() -> String {
     let mut your_name = String::new(); // Create new, empty string
     stdin()
         // Requires creating an empty string in order to save to it.
@@ -9,5 +16,5 @@ fn main() {
         // prefixing with & creates a reference to a variable.  This allows you to pass the reference instead of a copy
         .read_line(&mut your_name) 
         .expect("Failed to read line"); // Show this if it fails
-    println!("Hello, {}", your_name)
+    your_name // Return statement is the final item in a function
 }
