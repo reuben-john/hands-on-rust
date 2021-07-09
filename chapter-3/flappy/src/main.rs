bd84b582471ad50d3934afcba3c3ce4e3c7a9957
Reuben Koppenheffer <reuben.koppenheffer@accredopkg.com>
2021-07-09 15:44:29 -0500

    Add hit obstacle

--- chapter-3/flappy/src/main.rs
+++ chapter-3/flappy/src/main.rs
@@ -132,7 +132,7 @@ impl Obstacle {
         }
     }
 
-    fn render(&mut self, ctx: &mut BTerm, player_x : i32) {
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
         let screen_x = self.x - player_x;
         let half_size = self.size / 2;
 
@@ -145,6 +145,14 @@ impl Obstacle {
         for y in self.gap_y + half_size..SCREEN_HEIGHT {
             ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
         }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
     }
 }
 
