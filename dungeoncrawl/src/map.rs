55fc3135d6150f56fbe26f22d9669649f6bd2610
Reuben <reuben@koppenheffer.me>
2021-07-16 21:20:40 -0500

    Set up part of the map

---
+++ dungeoncrawl/src/map.rs
@@ -0,0 +1,9 @@
+use std::usize;
+
+use crate::prelude::*;
+const NUM_TILES: usize = {SCREEN_WIDTH * SCREEN_HEIGHT} as usize;
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor
+}
