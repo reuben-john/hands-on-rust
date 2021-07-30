536b00c5585375548095dd0b3cd7bd187d54466e
Reuben <reuben@koppenheffer.me>
2021-07-29 21:18:12 -0500

    Add map and render

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -9,17 +9,27 @@ mod prelude {
 
 use prelude::*;
 
+struct State {
+    map: Map,
+}
 
-struct State {}
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Welcome to Rusty Roguelike");
+        self.map.render(ctx)
     }
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50().with_title("Rusty Roguelike").build()?;
-    main_loop(context, State{})
+    let context = BTermBuilder::simple80x50()
+        .with_title("Rusty Roguelike")
+        .with_fps_cap(30.0)
+        .build()?;
+    main_loop(context, State::new())
 }
